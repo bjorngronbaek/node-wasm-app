27c78f57425673255c3e9eb761f2f18f03c2a1db
Bjørn Grønbæk <git@gronbaek.net>
2018-10-28 21:47:50 +0100

    inserted gol in JS app

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 //import * as wasm from "hello-wasm-pack";
-import * as wasm from "wasm-game-of-life";
+//import * as wasm from "wasm-game-of-life";
+import {Universe} from "wasm-game-of-life";
 
-wasm.greet("BG");
+const pre = document.getElementById("game-of-life");
+const universe = Universe.new();
+
+const renderLoop = () => {
+    pre.textContent = universe.render();
+    universe.tick();
+    requestAnimationFrame(renderLoop);
+}
+
+requestAnimationFrame(renderLoop);
